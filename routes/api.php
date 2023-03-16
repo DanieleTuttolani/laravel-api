7d2ebaae66a406b0038756e3b0a236dd1305c971
DanieleTuttolani <danieletuttolani2@gmail.com>
2023-03-16 14:28:23 +0100

    show api

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/index', [ProjectController::class, 'index']);
+Route::get('/project/{project}', [ProjectController::class, 'show']);
